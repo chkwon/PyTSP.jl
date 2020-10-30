31e2a8ee225f33780bd22992a5bcb9024c9ff5c5
Changhyun Kwon <chkwon@gmail.com>
2020-10-30 09:20:50 -0400

    update

--- example.jl
+++ example.jl
@@ -1,15 +1,16 @@
 include("tsp_solvers.jl")
 
 #  random TSP instance
-n_cities = 100
-x = rand(n_cities)
-y = rand(n_cities)
+n_cities = 300
+x = rand(n_cities) .* 100000
+y = rand(n_cities) .* 100000
 
 # The Concorde TSL Solver via pyconcorde
-time_concorde = @elapsed tour_concorde, length_concorde = solve_TSP(x, y, :Concorde)
+time_concorde = @elapsed tour_concorde, length_concorde = solve_TSP_Concorde(x, y, norm="EUC_2D")
+length_concorde_float = tour_length(tour_concorde, x, y)
 
 # The LKH Heuristic via elkai 
-time_LKH = @elapsed tour_LKH, length_LKH = solve_TSP(x, y, :LKH)
+time_LKH = @elapsed tour_LKH, length_LKH = solve_TSP_LKH(x, y, dtype="int")
 gap = (length_LKH - length_concorde) / length_LKH * 100 
 gap = round(gap * 100) / 100 
 
@@ -19,8 +20,8 @@ using Gurobi
 optimizer = Gurobi.Optimizer
 set_default_optimizer!(optimizer)
 cities = [ [x[i], y[i]] for i in 1:n_cities]
-time_jl = @elapsed tour_jl, cost_jl = get_optimal_tour(cities; verbose = true)
-length_jl = tour_length(tour_jl, x, y)
+time_jl = @elapsed tour_jl, length_jl = get_optimal_tour(cities; verbose = false)
+length_jl_float = tour_length(tour_jl, x, y)
 
 
 # Compare the solutions 
@@ -28,6 +29,6 @@ println("============ n_cities = $n_cities ======================")
 @show tour_concorde 
 @show tour_LKH 
 @show tour_jl 
-println("Concorde      Tour Length: $length_concorde ($time_concorde seconds)")
+println("Concorde      Tour Length: $length_concorde ($time_concorde seconds) (float length=$length_concorde_float)")
 println("LKH           Tour Length: $length_LKH ($time_LKH seconds) (gap = $gap %)")
-println("Julia Package Tour Length: $length_jl ($time_jl seconds)")
+println("Julia Package Tour Length: $length_jl ($time_jl seconds) (float length=$length_jl_float)")
--- tsp_solvers.jl
+++ tsp_solvers.jl
@@ -3,21 +3,32 @@ include("utils.jl")
 using PyCall
 using TravelingSalesmanExact
 
-function solve_TSP_Concorde(x, y)
+function solve_TSP_Concorde(x, y; norm="EUC_2D")
     Concorde = pyimport("concorde.tsp")
-    solver = Concorde.TSPSolver.from_data(x, y, norm="GEOM")
+    solver = Concorde.TSPSolver.from_data(x, y, norm)
     tour_data = solver.solve()
     tour_concorde = tour_data[1] .+ 1  # 0-index -> 1-index
     length_concorde = tour_length(tour_concorde, x, y) 
+    length_concorde = Int(tour_data[2])
     return tour_concorde, length_concorde
 end
 
-function solve_TSP_LKH(x, y)
+function solve_TSP_LKH(M; dtype="int")
     Elkai = pyimport("elkai")
-    M = construct_distance_matrix(x, y)
-    tour_LKH = Elkai.solve_float_matrix(M) .+ 1 #adding 1 for indexing
+    if dtype=="int" 
+        M = Int.(round.(M))
+        tour_LKH = Elkai.solve_int_matrix(M) .+ 1 #adding 1 for indexing
+    elseif dtype == "float" 
+        tour_LKH = Elkai.solve_float_matrix(M) .+ 1 #adding 1 for indexing
+    end
     length_LKH = tour_length(tour_LKH, M)
     return tour_LKH, length_LKH
+end
+
+function solve_TSP_LKH(x, y; dtype="int")
+    Elkai = pyimport("elkai")
+    M = construct_distance_matrix(x, y)
+    return solve_TSP_LKH(M, dtype=dtype)
 end
 
 function solve_TSP_JuliaExact(x, y, optimizer)
@@ -28,17 +39,3 @@ function solve_TSP_JuliaExact(x, y, optimizer)
     return tour_jl, length_jl
 end
 
-function solve_TSP(x, y, solver=:Concorde; optimizer=nothing)
-    if solver == :Concorde 
-        return solve_TSP_Concorde(x, y)
-    elseif solver == :LKH 
-        return solve_TSP_LKH(x, y)
-    elseif solver == :JuliaExact
-        if optimizer == nothing 
-            error("An optimizer, such as Gurobi.Optimizer, is required.")
-        end
-        return solve_TSP_JuliaExact(x, y, optimizer)
-    else 
-        error("Solver $solver is not available.")
-    end
-end
